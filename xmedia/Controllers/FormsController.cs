64335e681805b3e6ab9e2ad094d31270db8ace9e
joji espinoza <joji.espinoza@gmail.com>
2021-09-24 15:16:53 +0800

    edit

--- xmedia/Controllers/FormsController.cs
+++ xmedia/Controllers/FormsController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLogic.Forms;
 using BusinessRef.Interfaces.Forms;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Web.Mvc;
+using System;
+using BusinessRef.ModelObject.Forms;
 
 namespace xmedia.Controllers
 {
@@ -11,7 +15,35 @@ namespace xmedia.Controllers
         {
             IGetPurchaseOrderFormsData data = new GetPurchaseOrderDataLogic(1);
             var data1 = data.GetPurchaseOrderForms();
+
             return View("~/Views/Forms/Index.cshtml",data1);
+        }
+
+        public ActionResult PurchaseOrder1()
+        {
+            GetPurchaseOrderFormsInitialDataModel model = null;
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44308/api/");
+
+                var responseTask = client.GetAsync("forms/purchaseorder");
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<GetPurchaseOrderFormsInitialDataModel>();
+
+                    readTask.Wait();
+
+                    model = readTask.Result;
+                }
+            }
+
+            return View("~/Views/Forms/Index.cshtml", model);
         }
         
     }
