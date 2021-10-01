5c931cb3cd66bbcceaa516703f0aca4b7dd29097
joji espinoza <joji.espinoza@gmail.com>
2021-10-01 10:58:26 +0800

    push here

---
+++ BusinessRef/ModelObject/Orders/UserFormOrdersNoDuplicateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRef.ModelObject.Orders
+{
+    public class UserFormOrdersNoDuplicateModel
+    {
+        public int ID { get; set; }
+        public int OrderFormsCategory_ID { get; set; }
+        public int UsersID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float TotalPrice { get; set; }
+        public OrderFormsModel Orderforms { get; set; }
+    }
+}
